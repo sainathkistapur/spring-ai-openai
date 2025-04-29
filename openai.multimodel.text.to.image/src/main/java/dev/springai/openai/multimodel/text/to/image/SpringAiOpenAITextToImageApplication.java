fdcb4f97aed640ae8dc8ea525be621ca558dd81a
sainathkistapur@gmail.com <sainathkistapur@gmail.com>
2025-04-29 16:20:02 +0100

    Initial Push

---
+++ openai.multimodel.text.to.image/src/main/java/dev/springai/openai/multimodel/text/to/image/SpringAiOpenAITextToImageApplication.java
@@ -0,0 +1,13 @@
+package dev.springai.openai.multimodel.text.to.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOpenAITextToImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOpenAITextToImageApplication.class, args);
+	}
+
+}
---
+++ openai.multimodel.text.to.image/src/main/java/dev/springai/openai/multimodel/text/to/image/controller/TextToImageController.java
@@ -0,0 +1,36 @@
+package dev.springai.openai.multimodel.text.to.image.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.image.Image;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/springai/openai/multimodel")
+@RequiredArgsConstructor
+@Slf4j
+public class TextToImageController {
+
+    private final OpenAiImageModel openAiImageModel;
+    @GetMapping("/text-to-image")
+    public String generateImage(@RequestBody String prompt) {
+
+        ImagePrompt imagePrompt = new ImagePrompt(prompt,
+                OpenAiImageOptions.builder()
+                        .height(1024)
+                        .width(1024)
+                        .quality("hd")
+                        .withN(1)
+                        .build());
+
+        Image image = openAiImageModel.call(imagePrompt).getResult().getOutput();
+
+        return image.getUrl();
+    }
+}
