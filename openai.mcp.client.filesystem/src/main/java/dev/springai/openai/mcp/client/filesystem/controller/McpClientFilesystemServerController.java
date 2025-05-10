2c9bd25d61ac7983902426ee47ced67b89de69a3
sainathkistapur@gmail.com <sainathkistapur@gmail.com>
2025-05-10 09:29:09 +0100

    Rename

--- openai.mcp.client.filesystem/src/main/java/dev/springai/openai/mcp/client/filesystem/controller/McpClientFilesystemServerController.java
+++ openai.mcp.client.filesystem/src/main/java/dev/springai/openai/mcp/client/filesystem/controller/McpClientFilesystemServerController.java
@@ -1,22 +1,18 @@
 package dev.springai.openai.mcp.client.filesystem.controller;
 
-import io.modelcontextprotocol.client.McpSyncClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/springai/openai/mcp/client/filesystem")
 @RequiredArgsConstructor
 @Slf4j
-public class McpClientController {
+public class McpClientFilesystemServerController {
 
     private final ChatClient basicChatClient;
 
